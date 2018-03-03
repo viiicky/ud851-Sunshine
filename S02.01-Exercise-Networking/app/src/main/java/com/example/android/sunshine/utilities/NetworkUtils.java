569df3247e95e45c912ca0f67ef6f169ffd54bc1
viiicky <vikasprasad.prasad@gmail.com>
2018-03-03 20:21:14 +0530

    Solve S02.01

--- S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
+++ S02.01-Exercise-Networking/app/src/main/java/com/example/android/sunshine/utilities/NetworkUtils.java
@@ -15,9 +15,13 @@
  */
 package com.example.android.sunshine.utilities;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -65,8 +69,19 @@ public final class NetworkUtils {
      * @return The URL to use to query the weather server.
      */
     public static URL buildUrl(String locationQuery) {
-        // TODO (1) Fix this method to return the URL used to query Open Weather Map's API
-        return null;
+        URL url = null;
+        try {
+            url = new URL(Uri.parse(DYNAMIC_WEATHER_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARAM, locationQuery)
+                    .appendQueryParameter(FORMAT_PARAM, format)
+                    .appendQueryParameter(UNITS_PARAM, units)
+                    .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays)).build().toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.v(TAG, "Built URL: " + url);
+        return url;
     }
 
     /**
